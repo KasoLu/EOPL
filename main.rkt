f628e378d37abf90c8d7645c8e7bc4b451060457
Kaso.Lu <kasoalpha@gmail.com>
2019-02-14 10:11:17 +0800

    Exercise 6.8

--- main.rkt
+++ main.rkt
@@ -3,7 +3,6 @@
 (load "funcs.rkt")
 (require racket/trace)
 
-; run : String -> ExpVal
 (define (run str)
   (value-of-program
     (scan&parse str)))
@@ -13,24 +12,28 @@
   (cases program pgm
     [a-program [expr]
       (value-of/k expr (init-env)
-        (lambda (val)
-          (begin (eopl:printf "End of computation.~%") val)))]))
+        (lambda (type val)
+          (if (eqv? type 'cont)
+            (begin (eopl:printf "End of computation.~%") val)
+            (report-uncaught-exception val))))]))
 
 ; value-of/k : Exp x Env x Cont -> ExpVal
 (define (value-of/k expr env cont)
   (cases expression expr
     [const-exp [num]
-      (cont (num-val num))]
+      (cont 'cont (num-val num))]
     [var-exp [var]
-      (cont (apply-env env var))]
+      (cont 'cont (apply-env env var))]
     [proc-exp [vars body]
-      (cont (proc-val (procedure vars body env)))]
+      (cont 'cont (proc-val (procedure vars body env)))]
     [letrec-exp [names varss bodies letrec-body]
       (value-of/k letrec-body (extend-env-rec names varss bodies env) cont)]
     [zero?-exp [exp1]
       (value-of/k exp1 env
-        (lambda (val)
-          (cont (bool-val (zero? (expval->num val))))))]
+        (lambda (type val)
+          (if (eqv? type 'cont)
+            (cont type (bool-val (zero? (expval->num val))))
+            (cont type val))))]
     [let-exp [vars exps body]
       (if (null? vars)
         (value-of/k body env cont)
@@ -38,31 +41,90 @@
           (if (null? exps)
             (value-of/k body (extend-env vars (reverse vals) env) cont)
             (value-of/k (car exps) env
-              (lambda (val)
-                (loop (cdr exps) (reverse val vals)))))))]
+              (lambda (type val)
+                (if (eqv? type 'cont)
+                  (loop (cdr exps) (cons val vals))
+                  (cont type val)))))))]
     [if-exp [exp1 exp2 exp3]
       (value-of/k exp1 env
-        (lambda (val1)
-          (if (expval->bool val1)
-            (value-of/k exp2 env cont)
-            (value-of/k exp3 env cont))))]
+        (lambda (type val)
+          (if (eqv? type 'cont)
+            (if (expval->bool val)
+              (value-of/k exp2 env cont)
+              (value-of/k exp3 env cont))
+            (cont type val))))]
     [diff-exp [exp1 exp2]
       (value-of/k exp1 env
-        (lambda (val1)
-          (value-of/k exp2 env
-            (lambda (val2)
-              (cont (num-val (- (expval->num val1) (expval->num val2))))))))]
+        (lambda (type1 val1)
+          (if (eqv? type1 'cont)
+            (value-of/k exp2 env
+              (lambda (type2 val2)
+                (if (eqv? type2 'cont)
+                  (cont type2 (num-val (- (expval->num val1) (expval->num val2))))
+                  (cont type2 val2))))
+            (cont type1 val1))))]
+    [multi-exp [exp1 exp2]
+      (value-of/k exp1 env
+        (lambda (type1 val1)
+          (if (eqv? type1 'cont)
+            (value-of/k exp2 env
+              (lambda (type2 val2)
+                (if (eqv? type2 'cont)
+                  (cont type2 (num-val (* (expval->num val1) (expval->num val2))))
+                  (cont type2 val2))))
+            (cont type1 val1))))]
     [call-exp [rator rands]
       (value-of/k rator env
-        (lambda (rator-val)
-          (let loop([rands rands] [vals '()])
-            (if (null? rands)
-              (apply-proc/k (expval->proc rator-val) (reverse vals) cont)
-              (value-of/k (car rands) env
-                (lambda (rand-val)
-                  (loop (cdr rands) (cons rand-val vals))))))))]))
+        (lambda (type val)
+          (if (eqv? type 'cont)
+            (let loop([rands rands] [vals '()])
+              (if (null? rands)
+                (apply-proc/k (expval->proc val) (reverse vals) cont)
+                (value-of/k (car rands) env
+                  (lambda (rand-type rand-val)
+                    (if (eqv? rand-type 'cont)
+                      (loop (cdr rands) (cons rand-val vals))
+                      (cont rand-type rand-val))))))
+            (cont type val))))]
+    [list-exp [exps]
+      (let loop([exps exps] [vals '()])
+        (if (null? exps)
+          (cont 'cont (list-val (reverse vals)))
+          (value-of/k (car exps) env
+            (lambda (type val)
+              (if (eqv? type 'cont)
+                (loop (cdr exps) (cons val vals))
+                (cont type val))))))]
+    [car-exp [exp1]
+      (value-of/k exp1 env
+        (lambda (type val)
+          (if (eqv? type 'cont)
+            (cont type (car (expval->list val)))
+            (cont type val))))]
+    [cdr-exp [exp1]
+      (value-of/k exp1 env
+        (lambda (type val)
+          (if (eqv? type 'cont)
+            (cont type (list-val (cdr (expval->list val))))
+            (cont type val))))]
+    [null?-exp [exp1]
+      (value-of/k exp1 env
+        (lambda (type val)
+          (if (eqv? type 'cont)
+            (cont type (bool-val (null? (expval->list val))))
+            (cont type val))))]
+    [try-exp [exp1 var handler-exp]
+      (value-of/k exp1 env
+        (lambda (type val)
+          (if (eqv? type 'cont) 
+            (cont type val)
+            (value-of/k handler-exp (extend-env (list var) (list val) env) cont))))]
+    [raise-exp [exp1]
+      (value-of/k exp1 env
+        (lambda (type val)
+          (cont 'exception val)))]
+    ))
 
-; apply-proc/k : Proc x List(ExpVal) x Cont -> ExpVal
 (define (apply-proc/k proc1 vals cont)
   (cases proc proc1
     [procedure [vars body saved-env]
@@ -72,11 +134,4 @@
 ;(trace apply-proc/k)
 ;(trace value-of/k)
 ;(trace apply-cont)
-
-; res = (num-val 1)
-(define p1
-  "letrec
-     even(x) = if zero?(x) then 1 else (odd  -(x,1))
-     odd(x)  = if zero?(x) then 0 else (even -(x,1))
-   in (odd 13)")
 
